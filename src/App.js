383b6163e31bdd821108553af0c1ae6f98d5d7a1
David Wang <dw3fd2011@gmail.com>
2023-10-20 01:16:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
 import Link from "./components/link/link";
 const App = () => {
@@ -6,7 +5,6 @@ const App = () => {
     <>
       <Header />
       <Link />
-      <Footer />
     </>
   );
 };
