3657d0176f50e14bc37b6785881ee80907b0f996
David Wang <dw3fd2011@gmail.com>
2023-10-20 00:22:16 -0500

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -39,91 +39,21 @@ const linkData = [
     link: "https://steamcommunity.com/id/bairminer/",
     name: "Steam",
     image:
-      "https://i.imgur.com/c16rgFI.png",
+      "https://1000logos.net/wp-content/uploads/2020/08/Steam-Logo-800x500.png",
   },
   {
     id: 2,
     link: "https://www.youtube.com/@bairminer",
     name: "Youtube",
     image:
-      "https://en.m.wikipedia.org/wiki/File:YouTube_social_white_square_%282017%29.svg",
+      "https://1000logos.net/wp-content/uploads/2017/05/Red-YouTube-logo-768x432.png",
   },
   {
     id: 3,
     link: "https://open.spotify.com/user/bairminer",
     name: "Spotifyr",
     image:
-      "https://en.m.wikipedia.org/wiki/File:Spotify_icon.svg",
-  },
-  {
-    id: 4,
-    link: "https://go.vigneshcodes.in/bulkmailer",
-    name: "BulkMailer - Free Bulk Mailer App",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 5,
-    link: "https://tesla-clone-6820b.web.app",
-    name: "Tesla UI Clone Using ReactJS",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 6,
-    link: "https://web-scrapper-v2.herokuapp.com",
-    name: "Free S.E.O Scrapper / API",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 7,
-    link: "https://fervent-cray-ead198.netlify.app",
-    name: "Free Blogging Application",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 8,
-    link: "https://vignesh-blog.herokuapp.com",
-    name: "Free Blogging Application",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 9,
-    link: "https://quizzical-pike-e7098a.netlify.app",
-    name: "Open-Source React",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 10,
-    link: "https://linktree.vigneshcodes.in",
-    name: "Display Links Uniquely ",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 11,
-    link: "https://blog.vigneshcodes.in/quick-guide-to-deploy-using-docker",
-    name: "Quick Guide To Deploy Using Docker",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-    {
-    id: 12,
-    link: "https://blog.vigneshcodes.in/create-linktree-clone",
-    name: "Create Linktree Clone",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 13,
-    link: "https://blog.vigneshcodes.in/docker-for-production-build",
-    name: "Docker For Production Build",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
+      "https://1000logos.net/wp-content/uploads/2021/04/Spotify-logo-768x432.png",
   },
 ];
 
