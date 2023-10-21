71abfba3a2ac4eb6382c17246d03c2a1874d5657
David Wang <dw3fd2011@gmail.com>
2023-10-21 00:32:20 -0500

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -48,6 +48,55 @@ const linkData = [
     image:
       "https://upload.wikimedia.org/wikipedia/commons/9/9b/Spotify_1.png",
   },
+  {
+    id: 4,
+    link: "https://www.reddit.com/user/Bairminer",
+    name: "Reddit",
+    image:
+      "https://www.redditinc.com/assets/images/site/reddit-logo.png",
+  },
+  {
+    id: 5,
+    link: "https://twitter.com/Bairminer",
+    name: "X",
+    image:
+      "https://cdn.cms-twdigitalassets.com/content/dam/about-twitter/x/brand-toolkit/logo-black.png.twimg.2560.png",
+  },
+  {
+    id: 6,
+    link: "https://www.instagram.com/bairminer/",
+    name: "Instagram",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/58/Instagram-Icon.png",
+  },
+  {
+    id: 7,
+    link: "https://www.twitch.tv/bairminer",
+    name: "Twitch",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/7/74/Twitch-icon-5.png",
+  },
+  {
+    id: 8,
+    link: "https://pin.it/30hsMYm",
+    name: "Pinterest",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png",
+  },
+  {
+    id: 9,
+    link: "https://imgur.com/user/Bairminer/submitted",
+    name: "Imgur",
+    image:
+      "https://static.wikia.nocookie.net/logopedia/images/9/9f/Imgur_Icon_2018.png/",
+  },
+  {
+    id: 10,
+    link: "https://www.tumblr.com/bairminer",
+    name: "Tumblr",
+    image:
+      "https://static.wikia.nocookie.net/logopedia/images/8/88/TumblrIcon.jpg/",
+  },
 ];
 
 const footerData = {
